c21a6913c003ca472c520d076082229cdb55c84f
Noah Redford <noah.redford27@gmail.com>
2023-02-10 21:36:55 -0700

    created index.js functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,31 +1,41 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs')
+const generateMD = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [
-    {   
-        message: "What is the name of the project?",
-        name: "title"
+function questions() {
+    return inquirer.prompt([
+
+    {name: 'description',
+        message: 'What does this application do?',
+        type: 'input'
     },
     {   
         message: "What is a description of the project",
-        name: "description"
+        name: "input"
     },
     {   
         message: "How will this project be used?",
-        name: "use"
+        name: "input"
     },
     {   
         message: "What licesnes are used?",
-        name: "liscense"
+        name: "input"
     }
-];
+])};
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.appendFile(`${fileName}.md`, data,
+    (err) => err ? console.log(err) : console.log(`${fileName}.md has been generated.`))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    let answers = await questions();
+    writeToFile((answers.fileName), (generateMD(answers)));
+}
 
 // Function call to initialize app
 init();
