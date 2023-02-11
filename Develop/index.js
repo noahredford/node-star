ea4ca5a5b2944d87231bbee70d77e50b61721eeb
Noah Redford <noah.redford27@gmail.com>
2023-02-10 21:02:14 -0700

    copied starter code and added some quesitons to array

---
+++ Develop/index.js
@@ -0,0 +1,31 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {   
+        message: "What is the name of the project?",
+        name: "title"
+    },
+    {   
+        message: "What is a description of the project",
+        name: "description"
+    },
+    {   
+        message: "How will this project be used?",
+        name: "use"
+    },
+    {   
+        message: "What licesnes are used?",
+        name: "liscense"
+    }
+];
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
